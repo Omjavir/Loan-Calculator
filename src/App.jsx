090b85ac050c86887879ae0dd92b96e28d8c21ab
Om Suresh Javir <javirom07042003@gmail.com>
2023-03-30 16:01:11 +0530

    added preloader

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,11 @@ import { Container } from "@mui/system";
 import Result from "./Components/Result";
 import TenureSelect from "./Components/TenureSelect";
 import SliderSelect from "./Components/SliderSelect";
+import { Lines } from "react-preloaders";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
   const [data, setData] = useState({
     homeValue: 10000,
     downPayment: 3000 * 0.2,
@@ -15,8 +18,13 @@ function App() {
     interestRate: 5,
   });
 
+  setTimeout(() => {
+    setLoading(false)
+  }, 1000);
+
   return (
     <div>
+      <Lines customLoading={loading} />
       <Navbar />
       <Container maxWidth="xl" sx={{ marginTop: 4 }}>
         <Grid container spacing={5} alignItems="center">

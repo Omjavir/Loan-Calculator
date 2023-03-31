3c844c1adc800e9608fa8140eabf37af15a06cd9
Om Suresh Javir <javirom07042003@gmail.com>
2023-03-31 14:29:26 +0530

    added github link

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,13 +2,29 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import { Container } from "@mui/system";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import { Fab } from "@mui/material";
 
 const Navbar = () => {
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar>
-          <Typography variant="h5">Loan Calculator</Typography>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignContent: "center",
+              width: "100%",
+            }}
+          >
+            <h2>Loan Calculator</h2>
+            <div style={{ paddingTop: "20px" }}>
+              <a href="https://github.com/Omjavir/Loan-Calculator" style={{color:"inherit"}} target="_blank" rel="noopener noreferrer">
+                <GitHubIcon fontSize="large" />
+              </a>
+            </div>
+          </div>
         </Toolbar>
       </Container>
     </AppBar>
